9440a1a63c8e25e51e495180d4b44be17420d876
GaigolRam <ramgaigol9@gmail.com>
2025-08-03 10:44:21 +0530

    Add initial model class

---
+++ Bike_Rental_Web_Application/src/main/java/com/bike_rental_app/model/Role.java
@@ -0,0 +1,9 @@
+package com.bike_rental_app.model;
+
+public enum Role {
+	CUSTOMER,
+    ADMIN,
+    APPROVER,
+    OBSERVER,
+    SUPERUSER
+}
---
+++ Bike_Rental_Web_Application/src/test/java/com/bike_rental_app/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bike_rental_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
